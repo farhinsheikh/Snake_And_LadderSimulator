bcd57f84e124686643f15343047097a80920a858
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-06-10 23:51:14 +0530

    ADDED : initialize constants and option for moving position for snake and ladder

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -1,11 +1,26 @@
 #!/bin/bash -x
 echo "Welcome to Snake And Ladder Simulator Problem"
 
+POSITION=0
+SNAKE=1
+LADDER=2
+NO_PLAY=0
 player=1
-position=0
+
+positionOfPlayer=$POSITION
 
 function getDieRoll(){
 	die=$(( RANDOM % 6 + 1 ))
+	checkOptions=$(( RANDOM % 3 ))
+	case $checkOptions in
+		$NO_PLAY)
+			positionOfPlayer=$((positionOfPlayer+0));;
+		$LADDER)
+			positionOfPlayer=$((positionOfPlayer+die));;
+		$SNAKE)
+			positionOfPlayer=$((positionOfPlayer-die));;
+	esac
 }
 getDieRoll
-echo "your number is = $die"
+echo "POSITION OF PLAYER = $positionOfPlayer"
+

520a15f3540b9ce752164ad6557ec941a098161e
Farhin Sheikh <farhinsheikh2015@gmail.com>
2020-06-13 01:00:27 +0530

    REFACTOR : initialise variables and code for two players

--- SnakeAndLadderSimulator.sh
+++ SnakeAndLadderSimulator.sh
@@ -2,86 +2,74 @@
 echo "Welcome to Snake And Ladder Simulator Problem"
 
 START_POSITION=0
-SNAKE=1
-LADDER=2
 WIN_POSITION=100
+LADDER=1
+SNAKE=2
+
+positionOfPlayer1=0
+positionOfPlayer2=0
 dieNumber=0
-player=1
-positionOfPlayer=$START_POSITION
 
-declare -A positionDictionary
+function getDieRoll()
+{
 
-function getDieRoll(){
-	die=$(( RANDOM % 6 + 1 ))
-	checkOptions=$(( RANDOM % 3 ))
-	case $checkOptions in
-		$NO_PLAY)
-			positionOfPlayer=$((positionOfPlayer+0));;
-		$LADDER)
-			if [ $(( positionOfPlayer+die )) -gt $WIN_POSITION ]
-			then
-				positionOfPlayer=$((positionOfPlayer-die))
-			else
-				positionOfPlayer=$((positionOfPlayer+die))
-			fi;;
-		$SNAKE)
-			if [ $((positionOfPlayer-die)) -lt $START_POSITION ]
-			then
-				 positionOfPlayer=$START_POSITION
-         else
-             positionOfPlayer=$((positionOfPlayer-die))
-         fi;;
+   currentPosition=$1
+   die=$(( (( $RANDOM%6 ))+1 ))
+   checkOptions=$(( $RANDOM%3 ))
 
-	esac
+   case $checkOptions in
+      $NOPLAY)
+         currentPosition=$currentPosition ;;
+
+      $LADDER)
+         currentPosition=$(( $currentPosition+$die )) ;;
+
+      $SNAKE)
+         currentPosition=$(( $currentPosition-$die )) ;;
+
+   esac
+
+   if [[ $currentPosition -lt START_POSITION ]]
+   then
+   currentPosition=0
+   elif [[ $currentPosition -gt WIN_POSITION ]]
+   then
+   currentPosition=$(( $currentPosition-$die ))
+   fi
+
+   echo $currentPosition
 }
 getDieRoll
-echo "POSITION OF PLAYER = $positionOfPlayer"
 
-function winPosition100(){
-	while(true)
-	do
-		getDieRoll
-		(( dieNumber++ ))
-		positionDictionary["$dieNumber"]=$positionOfPlayer
-
-		if [ $positionOfPlayer -eq $WIN_POSITION ]
-		then
-		break;
-		elif [ $positionOfPlayer -lt $START_POSITION ]
-		then
-			positionOfPlayer=$START_POSITION
-		fi
-	done
+function twoPlayers()
+{
+	while [[ $positionOfPlayer1 -lt WIN_POSITION ]] && [[ $positionOfPlayer2 -lt WIN_POSITION ]]
+   do
+      positionOfPlayer1=$(getDieRoll $positionOfPlayer1)
+      echo "Position of player 1 = $positionOfPlayer1"
+      positionOfPlayer2=$(getDieRoll $positionOfPlayer2)
+      echo "Position of player 2 = $positionOfPlayer2"
+      dieNumber=$(( $dieNumber+1 ))
+   done
 }
-winPosition100
-echo "player winning position = $positionOfPlayer"
 
-function getDieKeyVlaue(){
-for key in ${!positionDictionary[@]}
-do
-   echo "Player position when die rolled $key time is : ${positionDictionary[$key]}"
-done
+function getWinPosition()
+{
+	while [[ $positionOfPlayer1 -lt WIN_POSITION ]] && [[ $positionOfPlayer2 -lt WIN_POSITION ]]
+   do
+      twoPlayers
+
+      if [[ $positionOfPlayer1 -eq WIN_POSITION ]]
+      then
+         echo "Player 1 Won "
+      break
+      elif [[ $positionOfPlayer2 -eq WIN_POSITION ]]
+      then
+         echo "Player 2 Won "
+      break
+      fi
+   done
 }
-getDieKeyVlaue
-
-echo "Total Die Rolls = $dieNumber"
-
-function twoPlayers(){
-   while [ true ]
-      do
-			if [ $(( player%2 )) -eq 1 ]
-			then
-			getDieRoll
-			(( player++ ))
-				if [ $positionOfPlayer -eq $WIN_POSITION ]
-				then
-				echo "Player 1 Won"
-				else
-				echo "Player 2 Won"
-				fi
-			fi
-			break
-		done
-}
-twoPlayers
+getWinPosition
+echo "Total die roll is "$dieNumber
 
